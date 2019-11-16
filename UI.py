4993973ed109e2df6d0772332c953426b61beb66
James <jlb838@student.bham.ac.uk>
2019-11-16 17:58:54 +0000

    Added some encodings

--- UI.py
+++ UI.py
@@ -28,6 +28,10 @@ class Window(Frame) :
         edit = Menu(menu)
         edit.add_command(label = 'Show Image', command=self.showBoard())
         edit.add_command(label='Show Text', command=self.showTxt)
+        edit.add_command(label = "Show Player1", command=self.showPlayer1())
+        edit.add_command(label = "Show Player2", command=self.showPlayer2())
+        edit.add_command(label = "Show Cross", command=self.showCross("(1,1)"))
+        edit.add_command(label = "Show Circle", command=self.showCircle("(1,1)"))
         menu.add_cascade(label='Edit', menu=edit)
         # self.variable = tkinter.Stringvar()
 
@@ -46,6 +50,59 @@ class Window(Frame) :
         img = Label(self, image=render)
         img.image = render
         img.place(x=0,y=0)
+    def showCross(self, option) :
+        load = Image.open("Cross.jpg")
+        newSize = (150, 150)
+        load = load.resize(newSize)
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        if (option == "(1,1)") :
+            img.place(x=300, y=220)
+        elif (option == "(2,1)") :
+            img.place(x=650, y=220)
+        elif (option == "(3,1)") :
+            img.place(x = 950, y = 220)
+        elif (option == "(1,2)") :
+            img.place(x = 300, y=420)
+        elif (option == "(2,2)") :
+            img.place(x=650, y=420)
+        elif (option == "(3,2)") :
+            img.place(x=950, y=420)
+        elif (option == "(1,3)") :
+            img.place(x=300, y=620)
+        elif (option == "(2,3)") :
+            img.place(x=650, y = 620)
+        else :
+            img.place(x=950, y=620)
+
+
+    def showCircle(self, option) :
+        load = Image.open("Nought.jpg")
+        newSize = (140, 140)
+        load = load.resize(newSize)
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image= render
+        if (option == "(1,1)") :
+            img.place(x=300, y=235)
+        elif (option == "(2,1)") :
+            img.place(x=650, y=235)
+        elif (option == "(3,1)") :
+            img.place(x=950, y=235)
+        elif (option == "(1,2)") :
+            img.place(x=300, y=435)
+        elif (option == "(2,2)") :
+            img.place(x=650, y=435)
+        elif (option == "(3,2)") :
+            img.place(x=950, y = 435)
+        elif (option == "(1,3)") :
+            img.place(x=300, y=600)
+        elif (option == "(2,3)") :
+            img.place(x=650, y=600)
+        else :
+            img.place(x=950, y=600)
+
 
     def showBoard(self) :
         load = Image.open("Board.jpg")
@@ -54,11 +111,19 @@ class Window(Frame) :
         render = ImageTk.PhotoImage(load)
         img = Label(self, image=render)
         img.image = render
-        img.place(x=100, y = 200)
+        img.place(x=200, y = 200)
 
     def showTxt(self) :
         text = Label(self, text='Hey there')
         text.pack()
+
+    def showPlayer1(self) :
+        text = Label(self, text='Player 1', font=(None, 20))
+        text.pack(side=LEFT)
+
+    def showPlayer2(self) :
+        text = Label(self, text='Player 2', font=(None, 20))
+        text.pack(side=RIGHT)
 
 OptionsPlayer1 = [
     "Select one",
@@ -100,7 +165,7 @@ player2Place = OptionMenu(root, variable4, *OptionsSpace1)
 player2Place.pack(side=RIGHT)
 
 # player1Option.pack(pady = 0)
-root.geometry("3000x900")
+root.geometry("3000x700")
 
 app = Window(root)
 
